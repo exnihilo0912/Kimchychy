73aa0b7ee2434f9af4fcfca1124b160ccbe77ae8
Exii710 - Macbook Pro <adam.emmanuel.pro@gmail.com>
2019-12-18 11:17:18 +0900

    [ADD] Basic files structure

---
+++ pages/dictionary.js
@@ -0,0 +1,11 @@
+
+export default class Dictionary extends React.Component{
+    render() {
+        return (
+            <section>
+                <h1>Dictionnaire</h1>
+                <p>page dictionnaire</p>
+            </section>
+        );
+    }
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import {Button} from 'antd';
+import 'antd/dist/antd.css';
+
+export default class Index extends React.Component {
+    render() {
+        return (
+            <section>
+                <h1>Titre</h1>
+                <p>court paragraphe</p>
+                <Button>Test button</Button>
+            </section>
+
+        );
+    }
+};
